65c4ceae88e023e90380c3ac88b3663e169a034b
Aidan Bane <aidanfbane@gmail.com>
2017-06-25 18:29:37 -0700

    (feat) Hello World Route added in server/index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const app = express()
 
 let PORT = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 let listener = app.listen(PORT, () => {
   console.log('Server is now listening on:', listener.address().port)
 })
