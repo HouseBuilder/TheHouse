34386b07520f5d7d77cc00e9de99e34f4e081d60
Aidan Bane <aidanfbane@gmail.com>
2017-06-27 11:08:51 -0700

    (feat) Added standardJS linter and updated config files

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,28 @@ module.exports = {
     path: path.join(`${__dirname}/dist`),
     filename: 'bundle.js'
   },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
   module: {
-    loaders: [
+    rules: [
+      {
+        // set up standard-loader as a preloader
+        enforce: 'pre',
+        test: /\.js/,
+        loader: 'standard-loader',
+        exclude: /(node_modules|bower_components|dist)/,
+        options: {
+          // Emit errors instead of warnings (default = false)
+          error: false,
+          // enable snazzy output (default = true)
+          snazzy: true,
+          // other config options to be passed through to standard e.g.
+          parser: 'babel-eslint'
+        }
+      },
       {
         test: /\.js/,
         exclude: /node_modules/,
