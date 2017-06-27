82880c0e470caffac4df19aa9013db332689f482
Aidan Bane <aidanfbane@gmail.com>
2017-06-27 11:09:37 -0700

    (refactor) Removed useless constructor

--- client/App.js
+++ client/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react'
 import { Nav } from './components/container'
 
 class App extends Component {
-  constructor (props) {
-    super(props)
-  }
   render () {
     return (
       <div>
