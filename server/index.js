de37d2dcc6a65dd134417df31f476b048e3534f4
Aidan Bane <aidanfbane@gmail.com>
2017-06-27 10:44:23 -0700

    (feat) dist dir added to static files

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ let PORT = process.env.PORT || 3000
 
 // static files
 app.use(express.static(path.join(__dirname, './../public')))
+app.use('/dist', express.static(path.join(__dirname, './../dist')))
 
 let listener = app.listen(PORT, () => {
   console.log('Server is now listening on:', listener.address().port)
